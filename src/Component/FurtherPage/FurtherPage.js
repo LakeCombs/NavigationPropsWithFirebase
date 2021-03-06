1e0e693f8626ad60909d4d7d8c6cfd8bebc9f237
LakeCombs <adesolaolamilekan2@gmail.com>
2021-03-06 19:33:26 +0100

    adding the furtherPage

--- src/Component/FurtherPage/FurtherPage.js
+++ src/Component/FurtherPage/FurtherPage.js
@@ -17,14 +17,52 @@ function FurtherPage() {
 	useEffect(() => {
 		getData();
 	}, []);
-	return <div>this is the page id:{id}
-	<p>
-		{data && data.title}
-		<img src={data.avatar}/>
-		
-		{/* hello lakes */}
-	</p>
-	</div>;
+	return (
+		<center>
+			<div style={{ height: "auto", width: "500px", border: "2px solid blue" }}>
+				<div
+					style={{
+						height: "auto",
+						width: "100%",
+						backgroundColor: "lightblue",
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						paddingLeft: "20px",
+						paddingRight: "20px"
+					}}>
+					{" "}
+					<img
+						style={{
+							height: "40px",
+							width: "40px",
+							borderRadius: "200px",
+							objectFit: "cover"
+						}}
+						src={data.avatar}
+					/>{" "}
+					<h4>{data.title}</h4>
+				</div>
+				<div>
+					<center>{data.title}</center>
+					<center>{data.paragraph}</center>
+					<center>
+						{" "}
+						<img
+							style={{
+								height: "40px",
+								width: "40px",
+								borderRadius: "200px",
+								objectFit: "cover"
+							}}
+							src={data.img1}
+						/>{" "}
+						<p>{data.desc}</p>
+					</center>
+				</div>
+			</div>
+		</center>
+	);
 }
 
 export default FurtherPage;
